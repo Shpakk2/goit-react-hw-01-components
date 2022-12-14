68837b653394fe5e2484b61a5de3de9c67587ee7
Andrii Shpak <SHPAKK2@GMAIL.COM>
2022-12-14 02:03:21 +0200

    firstTry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+
+import css from './App.module.css';
+
+import { Profile } from 'components/Profile/Profile';
+import user from './Profile/user'
+
+import { Statistics } from 'components/Statistics/Statistics';
+import data from './Statistics/data'
+
+import { FriendList } from 'components/FriendList/FriendList'
+import friends from './FriendList/friends'
+
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory'
+import transactions from './TransactionHistory/transactions'
+
+console.log(transactions)
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.app}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />  
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
+    
   );
 };
